1cd63a383943bded0441329af5f27f0bb5d55543
Tech_Query <shiy007@qq.com>
2018-03-15 00:16:08 +0800

    [ Optimize ]  Reduce the code of curry() by Function.prototype.bind()
    [ Add ]  EditorConfig & Git hook

--- index.js
+++ index.js
@@ -2,47 +2,42 @@
 
 exports.sum = function (first, second) {
 
-  return first + second;
+    return  first + second;
 };
 
 
 exports.isSpare = function (first, second) {
 
-  return  ((first !== 10)  &&  (first + second === 10))  ?  1  :  0;
+    return  ((first !== 10) && (first + second === 10))  ?  1  :  0;
 };
 
 
 exports.isStrike = function (first) {
 
-  return  (first === 10)  ?  1  :  0;
+    return  (first === 10)  ?  1  :  0;
 };
 
 
 exports.curry = function (origin) {
 
-  var count = origin.length;
+    var count = origin.length;
 
-  return  function curry() {
+    return  function curry() {
 
-    var param = Array.from( arguments );
-
-    return  (count > param.length) ?
-      function () {
-
-        param.push.apply(param, arguments);
-
-        return  curry.apply(this, param);
-      } :
-      origin.apply(this, arguments);
-  };
+        return  (count > arguments.length) ?
+            curry.bind.apply(
+                curry,  Array.prototype.concat.apply([ null ],  arguments)
+            ) :
+            origin.apply(this, arguments);
+    };
 };
 
 
 exports.round = function (this_first, this_second, next_one, next_two) {
 
-  return  this_first + this_second +
-    exports.isSpare(this_first, this_second) * next_one +
-    exports.isStrike(this_first, this_second) * (next_one + next_two);
+    return  this_first + this_second +
+        exports.isSpare(this_first, this_second) * next_one +
+        exports.isStrike(this_first, this_second) * (next_one + next_two);
 };
 
 
@@ -52,36 +47,36 @@ const score = [ ], stack = [ ];
 
 exports.hit = function hit(count) {
 
-  var length = score.push( count ), curry;
+    var length = score.push( count ),  curry;
 
-  if (length % 2) {
+    if (length % 2) {
 
-    stack.push(curry = curry_round( count ));
-  }
-
-  for (let i = stack.length - 1;  i >= 0;  i--) {
-    if (
-        (curry !== stack[i])  &&
-        (stack[i] instanceof Function)
-    ) {
-      stack[i] = stack[i]( count );
+        stack.push(curry = curry_round( count ));
     }
-  }
 
-  if ((length % 2)  &&  (count === 10)) {
+    for (let i = stack.length - 1;  i >= 0;  i--) {
+        if (
+            (curry !== stack[i])  &&
+            (stack[i] instanceof Function)
+        ) {
+            stack[i] = stack[i]( count );
+        }
+    }
 
-    hit( 0 );
-  }
+    if ((length % 2)  &&  (count === 10)) {
+
+        hit( 0 );
+    }
 };
 
 
 exports.scoreOf = function (index) {
 
-  return  stack[ index ];
+    return  stack[ index ];
 };
 
 
 exports.score = function () {
 
-  return  stack.slice(0, 10).reduce( exports.sum );
+    return  stack.slice(0, 10).reduce( exports.sum );
 };
